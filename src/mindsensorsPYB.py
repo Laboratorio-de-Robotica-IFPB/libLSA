90b0c9d26aa571ee3f9da2e923d423f1ccc8f57b
Arthur Ramalho <60481981+ramalhow@users.noreply.github.com>
2025-10-27 14:03:08 -0300

    cleanup before merge

--- src/mindsensorsPYB.py
+++ src/mindsensorsPYB.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.iodevices  import I2CDevice
-from pybricks.parameters import Port
 
 LSA_DEFAULT_ADDRESS         = const(0x14)
-
 LSA_COMMAND                 = const(0x41)
-
 LSA_CALIBRATED              = const(0x42)
-
 LSA_UNCALIBRATED            = const(0x6A)
-
 LSA_WHITE_LIMIT             = const(0x4A)
-
 LSA_BLACK_LIMIT             = const(0x52)
-
 LSA_WHITE_CALIBRATION_DATA  = const(0x5A)
-
 LSA_BLACK_CALIBRATION_DATA  = const(0x62)
 
 class LightSensorArray:
     device_address = 0
 
-    def __init__(self, port, address=LSA_DEFAULT_ADDRESS):
+    def __init__(self, port):
         self.port = port
-        self.device_addres = address
-        self.i2c_device = I2CDevice(port, address>>1)
+        self.device_addres = LSA_DEFAULT_ADDRESS
+        self.i2c_device = I2CDevice(port, LSA_DEFAULT_ADDRESS>>1)
 
     def get_data(self, address, size) -> bytearray | None:
         try:
@@ -37,23 +28,15 @@ class LightSensorArray:
     def send_command(self, cmd) -> None:
         self.i2c_device.write(reg=LSA_COMMAND, data=cmd)
 
-    ## Calibrates the white value for the LightSensorArray
-    #  @param self The object pointer.
     def calibrate_white(self) -> None:
         self.send_command(b'W')
 
-    ## Calibrates the black value for the LightSensorArray
-    #  @param self The object pointer.
     def calibrate_black(self) -> None:
         self.send_command(b'B')
 
-    ## Wakes up or turns on the LEDs of the LightSensorArray
-    #  @param self The object pointer.
     def wakeup(self) -> None:
         self.send_command(b'P')
 
-    ## Puts to sleep, or turns off the LEDs of the LightSensorArray
-    #  @param self The object pointer.
     def goto_sleep(self) -> None:
         self.command(b'D')
 
@@ -62,11 +45,7 @@ class LightSensorArray:
 
     def get_raw_voltages(self) -> bytearray | None:
         s = self.get_data(LSA_UNCALIBRATED, 16)
-        array = []
-        for i in range(0, 16, 2):
-            array.append( int( s[i:(i+1)] ) )
-
-        #array = [ int(s[0:1]), s[2:3], s[4:5], s[6:7], s[8:9], s[10:11], s[12:13], s[14:15] ]
+        array = [ int(s[0:1]), s[2:3], s[4:5], s[6:7], s[8:9], s[10:11], s[12:13], s[14:15] ]
         return array
 
     def get_white_limit(self) -> bytearray | None:
